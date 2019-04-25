0488166b171714a9b45c4b6508576d1096bfd4d7
kmhosny <kmhosny@yahoo.com>
2019-04-25 18:44:23 +0200

    specs for message worker

--- test/workers/message_write_worker_test.rb
+++ test/workers/message_write_worker_test.rb
@@ -1,6 +1,18 @@
-require_relative 'test_helper'
+require_relative '../test_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.inline!
 class MessageWriteWorkerTest < MiniTest::Unit::TestCase
   def test_example
-    skip "add some examples to (or delete) #{__FILE__}"
+    app = App.create(name: 'CronJobApp')
+    chat = Chat.create(app: app)
+    chats_count = chat.messages.count
+    mc = MessageCache.create(app_id: app.id, chat_id: chat.id, body: 'Message Cache CronJob', written: false)
+    msg_count = Message.count
+    MessageWriteWorker.perform_async
+    assert_equal msg_count + 1, Message.count
+    mc.reload
+    assert_equal mc.written, true
+    chat.reload
+    assert_equal chat.messages.count, chats_count + 1
   end
 end
